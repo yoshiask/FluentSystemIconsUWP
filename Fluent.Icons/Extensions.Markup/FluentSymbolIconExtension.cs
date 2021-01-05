f1aeee59600d958d6c5b9e8daf3ffc1afb964c11
Joshua Askharoun <jjask7@gmail.com>
2021-01-04 19:46:57 -0600

    Add basic FluentSymbolIconExtension (crashes at runtime)

---
+++ Fluent.Icons/Extensions.Markup/FluentSymbolIconExtension.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Fluent.Icons.Extensions.Markup
+{
+    /// <summary>
+    /// Custom <see cref="MarkupExtension"/> which can provide symbol-based <see cref="FontIcon"/> values.
+    /// </summary>
+    [MarkupExtensionReturnType(ReturnType = typeof(FontIcon))]
+    public class SymbolIconExtension : TextIconExtension
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="FluentSymbol"/> value representing the icon to display.
+        /// </summary>
+        public FluentSymbol Symbol { get; set; }
+
+        /// <inheritdoc/>
+        protected override object ProvideValue()
+        {
+            var fontIcon = new FluentSymbolIcon(Symbol);
+
+            //if (FontSize > 0)
+            //{
+            //    fontIcon.FontSize = FontSize;
+            //}
+
+            //if (Foreground != null)
+            //{
+            //    fontIcon.Foreground = Foreground;
+            //}
+
+            return fontIcon;
+        }
+    }
+}
---
+++ Fluent.Icons/Extensions.Markup/TextIconExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+namespace Fluent.Icons.Extensions.Markup
+{
+    /// <summary>
+    /// An abstract <see cref="MarkupExtension"/> which to produce text-based icons.
+    /// </summary>
+    public abstract class TextIconExtension : MarkupExtension
+    {
+        /// <summary>
+        /// Gets or sets the size of the icon to display.
+        /// </summary>
+        public double FontSize { get; set; }
+
+        [ThreadStatic]
+        private static FontFamily segoeMDL2AssetsFontFamily;
+
+        /// <summary>
+        /// Gets the reusable "Segoe MDL2 Assets" <see cref="FontFamily"/> instance.
+        /// </summary>
+        protected static FontFamily SegoeMDL2AssetsFontFamily
+        {
+            get => segoeMDL2AssetsFontFamily ??= new FontFamily("Segoe MDL2 Assets");
+        }
+
+        /// <summary>
+        /// Gets or sets the thickness of the icon glyph.
+        /// </summary>
+        public FontWeight FontWeight { get; set; } = FontWeights.Normal;
+
+        /// <summary>
+        /// Gets or sets the font style for the icon glyph.
+        /// </summary>
+        public FontStyle FontStyle { get; set; } = FontStyle.Normal;
+
+        /// <summary>
+        /// Gets or sets the foreground <see cref="Brush"/> for the icon.
+        /// </summary>
+        public Brush Foreground { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether automatic text enlargement, to reflect the system text size setting, is enabled.
+        /// </summary>
+        public bool IsTextScaleFactorEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the icon is mirrored when the flow direction is right to left.
+        /// </summary>
+        public bool MirroredWhenRightToLeft { get; set; }
+    }
+}
